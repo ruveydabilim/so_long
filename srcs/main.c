49527aeb5918b538d6b5ccaf31033f1c23a3e605
Ruveyda BİLİM <ruveydabilimm@gmail.com>
2025-10-12 23:55:07 +0300

    fix: map_parser fixed.

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: rbilim <rbilim@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/12 22:23:10 by rbilim            #+#    #+#             */
-/*   Updated: 2025/10/12 23:11:33 by rbilim           ###   ########.fr       */
+/*   Updated: 2025/10/12 23:26:24 by rbilim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,9 @@
 
 int main(int argc, char **argv)
 {
+	char	**map;
 	if (argc != 2)
 		return(ft_printf("error! invalid arguman count."), 1);
-	
+	map = map_parser(argv[1]);
 	
 }
--- srcs/map_parser.c
+++ srcs/map_parser.c
@@ -6,21 +6,33 @@
 /*   By: rbilim <rbilim@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/12 22:44:48 by rbilim            #+#    #+#             */
-/*   Updated: 2025/10/12 23:10:52 by rbilim           ###   ########.fr       */
+/*   Updated: 2025/10/12 23:51:56 by rbilim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/so_long.h"
 
+static int	is_valid(char *arg)
+{
+	int	len;
+
+	len = ft_strlen(arg);
+	if (arg[len - 1] == 'r' && arg[len - 2] == 'e' 
+		&& arg[len - 3] == 'b' && arg[len - 4] == '.')
+			return (1);
+	else
+		return (0);
+}
+
 char	**map_parser(char *arg)
 {
-	size_t	fd;
+	char	*line;
 	char	**map;
 	int		i;
-	char	*line;
+	size_t	fd;
 
-	if (!arg || !*arg)
-		return (NULL);
+	if (!arg || !*arg || !is_valid(arg))
+		return (write(2, "error: invalid agrument type.\n", 53), NULL);
 	i = 0;
 	fd = open(arg, RDWR);
 	while (1)
